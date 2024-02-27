49f0b26f503061d4a636daab157d83ff41f46cea
Tanasoaia Ana-Maria <tanasoaiaanamaria@gmail.com>
2024-02-27 18:56:09 +0200

    [TASK-371] - initial codebase

---
+++ src/main/java/com/usv/Team/Finder/App/TeamFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.usv.Team.Finder.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamFinderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usv/Team/Finder/App/TeamFinderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usv.Team.Finder.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamFinderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
